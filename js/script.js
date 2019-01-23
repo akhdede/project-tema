77bf5f10886138efc1a3cd0ff737a10169d0effb
akhdede <akhdede@gmail.com>
2019-01-24 00:28:49 +0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-$('.page-scroll').on('click', function() {
+var sc = $.noConflict(); // scroll animation
 
-  var tujuan = $(this).attr('href');
+sc(document).ready(function() {
+  sc(".page-scroll").click(function(e) {
+    var tujuan = sc(this).attr("href");
 
-  var elemenTujuan = $(tujuan);
+    var elemenTujuan = sc(tujuan);
 
+    sc("#home").animate({
+        scrollTop: elemenTujuan.offset().top - 60
+      }, 1250, "swing");
+
+    e.preventDefault();
+  });
+
+  sc(window).scroll(function() {
+    var y = sc(this).scrollTop();
+
+    if(y > sc('#blog').offset().top - 100) {
+      sc("header nav").fadeIn(function() {
+        sc(this).addClass('navbar navbar-expand-lg navbar-dark bg-dark fixed-top');
+      });
+    }
+    else {
+      sc("header nav").fadeIn(function() {
+        sc(this).removeClass('bg-dark fixed-top');
+      });
+    }
+  });
 });
